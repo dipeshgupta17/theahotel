fcb71145d0e6446c1a0d109c813acce03c90197f
dipesh gupta <dipeshgupta17@gmail.com>
2019-08-04 23:02:34 +0530

    Initial Commit, create d routing module, added hotel pages, added paths

---
+++ src/app/app.module.ts
@@ -0,0 +1,61 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MaterialModule} from './material.module';
+import { AccommodationComponent } from './accommodation/accommodation.component';
+import { RoomsComponent } from './accommodation/rooms/rooms.component';
+
+import { BlogsComponent } from './blogs/blogs.component';
+
+import { BarsComponent } from './food-and-beverage/bars/bars.component';
+import { LifestyleComponent } from './lifestyle/lifestyle.component';
+import { WellnessAndSpaComponent } from './lifestyle/wellness-and-spa/wellness-and-spa.component';
+import { ResortActivitiesComponent } from './lifestyle/resort-activities/resort-activities.component';
+import { ExperiencesComponent } from './lifestyle/experiences/experiences.component';
+import { ExcursionsComponent } from './lifestyle/excursions/excursions.component';
+import { WeddingsComponent } from './weddings/weddings.component';
+import { ThaiWeddingsComponent } from './weddings/thai-weddings/thai-weddings.component';
+import { WesternsWeddingsComponent } from './weddings/westerns-weddings/westerns-weddings.component';
+import { CelebrationsComponent } from './celebrations/celebrations.component';
+import { OffersComponent } from './offers/offers.component';
+import { AuthComponent } from './auth/auth.component';
+import { SigninComponent } from './auth/signin/signin.component';
+import { SignupComponent } from './auth/signup/signup.component';
+import { FoodAndBeverageComponent } from './food-and-beverage/food-and-beverage.component';
+import { RestaurantsComponent } from './food-and-beverage/restaurants/restaurants.component';
+@NgModule({
+  declarations: [
+    AppComponent,
+    AccommodationComponent,
+    RoomsComponent,
+    BlogsComponent,
+    BarsComponent,
+    LifestyleComponent,
+    WellnessAndSpaComponent,
+    ResortActivitiesComponent,
+    ExperiencesComponent,
+    ExcursionsComponent,
+    WeddingsComponent,
+    ThaiWeddingsComponent,
+    WesternsWeddingsComponent,
+    CelebrationsComponent,
+    OffersComponent,
+    AuthComponent,
+    SigninComponent,
+    SignupComponent,
+    FoodAndBeverageComponent,
+    RestaurantsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
